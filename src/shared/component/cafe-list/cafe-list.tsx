fa8149b6e8563fae63d7da0c352f66ef9e7d8457
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-16 03:02:04 +0600

    feat: project set up

--- src/shared/component/cafe-list/cafe-list.tsx
+++ src/shared/component/cafe-list/cafe-list.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 import { CafeListCard } from './style/cafe-list.style';
 import Typography from '../typography/typography';
 
