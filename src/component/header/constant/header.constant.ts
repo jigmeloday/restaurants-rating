1d96138ec142b00498ce0b5a117d6a021d071fb5
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-16 03:53:00 +0600

    feat: project set up

---
+++ src/component/header/constant/header.constant.ts
@@ -0,0 +1,16 @@
+import { NavModel } from '../model/header.model';
+
+export const HEADER_NAV: NavModel[] = [
+    {
+        label: 'List',
+        path: '/'
+    },
+    {
+        label: 'Visited',
+        path: ''
+    },
+    {
+        label: 'Collections',
+        path: ''
+    }
+]
