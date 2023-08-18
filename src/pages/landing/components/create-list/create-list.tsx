9515b2813ce95b6370df794944e2b7fd3685cf17
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-18 21:56:05 +0600

    feat: open create list model

--- src/pages/landing/components/create-list/create-list.tsx
+++ src/pages/landing/components/create-list/create-list.tsx
@@ -1,6 +1,17 @@
+import { Box, Grid } from '@mui/material';
+import Typography from '../../../../shared/component/typography/typography';
+import Input from '../../../../shared/component/input/input.component';
+
 function CreateList() {
     return(
-        <>hello</>
+        <Grid container item py='24px' px='32px'>
+            <Box py='12px'>
+                <Typography label='Create List' variant='body1' />
+            </Box>
+            <Grid item container my='24px'>
+                <Input name='name' />
+            </Grid>
+        </Grid>
     )
 }
 
