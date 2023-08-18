49743aad66c35ff3e02d97751693d9e313816950
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-19 01:41:31 +0600

    feat: new cafe listing

--- src/pages/landing/schema/auth.schema.ts
+++ src/pages/landing/schema/auth.schema.ts
@@ -1,10 +1,6 @@
 import * as Yup from 'yup';
 import {
-    EMAIL_FIELD,
-    PASSWORD_LENGTH,
-    PASSWORD_REQUIRED_LOWERCASE,
-    PASSWORD_REQUIRED_NUMBER,
-    PASSWORD_REQUIRED_UPPERCASE, REQUIRED_FIELD
+    REQUIRED_FIELD
 } from '../../../shared/constant/shared.constant';
 
 export const CREATE_LIST_SCHEMA = Yup.object().shape( {
