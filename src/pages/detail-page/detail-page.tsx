bda107a22aa688ec19d3598d8f8745946951d3fa
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-20 17:03:14 +0600

    fixes

--- src/pages/detail-page/detail-page.tsx
+++ src/pages/detail-page/detail-page.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { CafeDetailsAPI } from '../landing/services/landing.api';
 import { Box, Chip, Grid } from '@mui/material';
 import Typography from '../../shared/component/typography/typography';
+import CafeList from '../../shared/component/cafe-list/cafe-list';
+import Button from '../../shared/component/button/button.component';
 
 function DetailPage() {
     const id = useParams()['id']
@@ -19,10 +21,10 @@ function DetailPage() {
             </Grid>
             <Grid item container my='12px' direction='column'>
                 <Typography label={detail?.name} variant='body1' />
-                <Typography label={detail.rating} variant='body1' />
-                <Typography label={detail.feedback} variant='body1' />
+                <Typography label={detail?.rating} variant='body1' />
+                <Typography label={detail?.feedback} variant='body1' />
                 {
-                    detail?.menu?.map((item: string, index: number) => (
+                   detail?.menu.length && detail?.menu?.map((item: string, index: number) => (
                        <Box key={index} my='24px'>
                            <Chip
                                label={item}
@@ -31,6 +33,22 @@ function DetailPage() {
                        </Box>
                     ))
                 }
+            </Grid>
+            <Grid item container my='24px' p='18px' boxShadow='rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;'>
+                <Typography label='Our Collection' variant='body1' fontWeight='700'/>
+                <Grid item container direction='row' gap='14px' pt='24px'>
+                    {
+                        detail?.collection?.length &&
+                        detail?.collection?.map((item: any) => (
+                            <Box key={item}>
+                                <CafeList item={item}/>
+                            </Box>
+                        ))
+                    }
+                </Grid>
+                <Grid item container mt='24px' justifyContent='center'>
+                    <Button label='Upload Collections' variant='outlined' />
+                </Grid>
             </Grid>
         </Grid>
     )
--- src/shared/component/cafe-list/cafe-list.tsx
+++ src/shared/component/cafe-list/cafe-list.tsx
@@ -14,27 +14,29 @@ function CafeList(props: any) {
         <Grid container item>
             <CafeListCard>
                 <Grid item container width='200px' height='200px'>
-                    <img src={props?.item?.cover} className='object-fit--cover' height='100%' width='100%'/>
+                    <img src={props?.item?.cover || props?.item} className='object-fit--cover' height='100%' width='100%'/>
                 </Grid>
-                <Grid item container direction='column' pt='4px'>
-                    <Typography label={`Place: ${props?.item?.name}`} variant='caption' />
-                    <Grid item container direction='row' justifyContent='space-between'>
-                        <Grid item container xs={6}>
-                            <Typography label={`Rating: ${props?.item?.rating || 0}/5`} variant='caption' />
+                {
+                  props?.item?.name && <Grid item container direction='column' pt='4px'>
+                        <Typography label={`Place: ${props?.item?.name}`} variant='caption' />
+                        <Grid item container direction='row' justifyContent='space-between'>
+                            <Grid item container xs={6}>
+                                <Typography label={`Rating: ${props?.item?.rating || 0}/5`} variant='caption' />
+                            </Grid>
+                            <Grid item container xs={6} justifyContent='end'>
+                                {
+                                    props?.item?.visited ?  <Icon iconName='tour' color={theme.palette.primary.main}/> : null
+                                }
+                                <Box pl='12px' className='cursor--pointer'>
+                                    <Icon iconName='favorite_outlined' color={theme.palette.primary.main}  />
+                                </Box>
+                            </Grid>
                         </Grid>
-                        <Grid item container xs={6} justifyContent='end'>
-                            {
-                                props?.item?.visited ?  <Icon iconName='tour' color={theme.palette.primary.main}/> : null
-                            }
-                            <Box pl='12px' className='cursor--pointer'>
-                                <Icon iconName='favorite_outlined' color={theme.palette.primary.main}  />
-                            </Box>
+                        <Grid item container mt='12px' xs={12}>
+                            <Button variant='outlined' click={() => route(`detail/${props?.item?.id}`)}  label='Details' className='width--full'/>
                         </Grid>
                     </Grid>
-                    <Grid item container mt='12px' xs={12}>
-                        <Button variant='outlined' click={() => route(`detail/${props?.item?.id}`)}  label='Details' className='width--full'/>
-                    </Grid>
-                </Grid>
+                }
             </CafeListCard>
         </Grid>
     )
